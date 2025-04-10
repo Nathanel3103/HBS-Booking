be4b28e57531482329f8f75e9b7051b09cf8be10
musukutwanick <musukutwanick@gmail.com>
2025-04-10 15:23:36 +0200

    Hid the role drop down menu on the register page

--- pages/register.js
+++ pages/register.js
@@ -207,16 +207,16 @@ export default function Register() {
             </div>
           </div>
 
-          <div>
-            <label className="block text-sm font-medium mb-1 text-gray-700">
+          <div classname ="hidden">
+            <label className="block text-sm font-medium mb-1 text-gray-700 hidden">
               Role
             </label>
             <div className="relative">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <UserGroupIcon className="h-5 w-5 text-gray-400" />
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none ">
+                <UserGroupIcon className="h-5 w-5 text-gray-400 hidden" />
               </div>
               <select
-                className="w-full pl-10 px-4 py-2 rounded-lg border border-gray-200 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                className="w-full pl-10 px-4 py-2 rounded-lg border border-gray-200 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-400 hidden"
                 value={role}
                 onChange={(e) => setRole(e.target.value)}
               >
